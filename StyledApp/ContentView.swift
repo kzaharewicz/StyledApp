22fd2a571818ac42e119c5b99aa854714e4c1bd9
Kate <kszaharewicz@gmail.com>
2024-07-16 11:30:28 -0400

    just created project

--- StyledApp/ContentView.swift
+++ StyledApp/ContentView.swift
@@ -11,7 +11,6 @@ struct ContentView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
-                .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
         }
