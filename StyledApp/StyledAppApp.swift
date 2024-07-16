0bc909ef15b76b0a3fc5c1f284e4ba0c06ff5fa0
Kate <kszaharewicz@gmail.com>
2024-07-16 11:24:42 -0400

    Initial Commit

---
+++ StyledApp/StyledAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StyledAppApp.swift
+//  StyledApp
+//
+//  Created by Scholar on 7/16/24.
+//
+
+import SwiftUI
+
+@main
+struct StyledAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
